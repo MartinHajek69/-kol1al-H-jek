b4cbb7b82d21a7edf2972365773b22c58bcfbad6
MartinHajek69 <159123515+MartinHajek69@users.noreply.github.com>
2024-11-06 09:25:57 +0000

    final_hajek

--- mhajek.py
+++ mhajek.py
@@ -1,1 +1,12 @@
-pole = ["oheň", "voda", "vítr", "kámen", "příroda"]
+#pole ktere ma 5 stringu
+pole = ["oheň", "voda", "blesky", "kámen", "příroda"]
+print("základní pole", pole)
+#přidání prvku vítr
+pole.append("vítr")
+print("Po přidání větru", pole)
+#odstrani vodu
+pole.remove("voda")
+print("Pole po odstranění druhého prvku:", pole)
+#změní přírodu na země
+pole[4] = "země"
+print("Pole po změně 5. hodnoty", pole)
